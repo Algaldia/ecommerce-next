4ab92297cbe7c8169e6326da23ee5a037359bf59
diegojsa <diegojsaraujo@gmail.com>
2024-04-23 10:04:18 -0300

    upando no github

---
+++ src/app/admin/products/_components/ProductActions.tsx
@@ -0,0 +1,43 @@
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { useTransition } from "react";
+import { toogleProductAvailability } from "../../_actions/products";
+
+export function ActiveToogleDropdownItem({ 
+    id, isAvaliableForPurchase 
+} : { 
+    id: string, 
+    isAvaliableForPurchase : boolean 
+}) {
+    const [isPendeng, startTransition] = useTransition()
+    return (
+        <DropdownMenuItem 
+            disabled={isPendeng}
+            onClick={() => {
+                startTransition(async () => {
+                    await toogleProductAvailability(id, !isAvaliableForPurchase)
+                })
+        }}>
+            {isAvaliableForPurchase ? "Deactivate" : "Activate"}
+        </DropdownMenuItem>
+    )
+}
+
+export function DeleteDropdownItem({
+    id, disabled
+} : {
+    id: string,
+    disabled: boolean
+}){
+    const [isPendeng, startTransition] = useTransition()
+    return (
+        <DropdownMenuItem 
+            disabled={disabled || isPendeng}
+            onClick={() => {
+                startTransition(async () => {
+                    await toogleProductAvailability(id, !isAvaliableForPurchase)
+                })
+        }}>
+            {isAvaliableForPurchase ? "Deactivate" : "Activate"}
+        </DropdownMenuItem>
+    )
+}
